55718d7952a9a4eb058535c205e22a82661f7641
kaushiki <kaushiki.ak88@gmail.com>
2021-02-26 22:46:47 +0530

    ADDED: to insert element between list

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -1,25 +1,49 @@
-public class LinkedList {
-    Node head,tail;
+class LinkedList {
+    Node head, tail;
     int size;
 
     public void append(int data) {
         Node node = new Node(data);
-        if(head == null) {
+
+        //CHECK IF LIST IS EMPTY
+        if (head == null) {
             head = node;
             tail = node;
-        }
-        else {
+        } else {
             tail.next = node;
             tail = node;
         }
+        size++;
     }
 
+    //TO ADD NEW NODE IN MIDDLE
+    public void addInMid(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp, current;
+            int count = (size % 2 == 0) ? (size / 2) : ((size + 1) / 2);
+            temp = head;
+            current = null;
+            for (int i = 0; i < count; i++) {
+                current = temp;
+                temp = temp.next;
+            }
+            current.next = newNode;
+            newNode.next = temp;
+        }
+        size++;
+    }
+    //TO DISPLAY ELEMENTS
     public void show() {
         Node n = head;
         while (n.next != null) {
-            System.out.println("Data : " + n.data);
-            n = n.next;
-        }
         System.out.println("Data : " + n.data);
+        n = n.next;
+    }
+    System.out.println("Data : " + n.data);
     }
 }
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -8,9 +8,9 @@ class Node{
 
     public static void main(String[] args) {
         LinkedList add = new LinkedList();
-        add.append(70);
-        add.append(30);
         add.append(56);
+        add.append(70);
+        add.addInMid(30);
         add.show();
     }
 }
